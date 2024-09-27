d32754fe0ae7d5f3abdc3cfe0848ba7b6915205c
Tuncay KAYA <tuncay.kaya@powercoders.org>
2024-09-27 10:12:43 +0200

    Added exercise Ex-4-1-1 and some class works

---
+++ week-2/Exercises/JavaScript/Ex-4-1-1.js
@@ -0,0 +1,103 @@
+/*Ex. 4.1.1 (*) - Play with data types, operations and comparisons
+Guess what answers you would get if you ran this in the JavaScript Console of your browser. Once you have
+an answer to the questions check the answers by copying them and running it in the console yourself line by
+line
+
+a) Evaluate the below:
+• 5 + "34"
+• 5 - "4"
+• 10 % 5
+• 5 % 10
+• "Java" + "Script"
+• " " + " "
+• " " + 0
+• true + true
+• true + false
+• false + true
+• false - true
+• 3 - 4
+• "Bob" - "bill"
+b) Evaluate the below comparisons:
+• 5 >= 1
+• 0 === 1
+• 4 <= 1
+• 1 != 1
+• "A" > "B"
+• "B" < "C"
+• "a" > "A"
+• "b" < "A"
+• true === false
+• true != true
+c) Make the string: "Hi There! It's "sunny" out" by using the + sign*/
+
+var result1 = 5 + "34";
+console.log("Result of result1 is " + result1);
+
+var result2 = 5 - "4";
+console.log("Result of result2 is " + result2);
+
+var result3 = 10 % 5;
+console.log("Result of result3 is " + result3);
+
+var result4 = 5 % 10;
+console.log("Result of result4 is " + result4);
+
+var result5 = "Java" + "Script";
+console.log("Result of result5 is " + result5);
+
+var result6 = " " + " ";
+console.log("Result of result6 is " + result6);
+
+var result7 = " " + 0;
+console.log("Result of result7 is " + result7);
+
+var result8 = true + true;
+console.log("Result of result8 is " + result8);
+
+var result9 = true + false;
+console.log("Result of result9 is " + result9);
+
+var result10 = false + true;
+console.log("Result of result10 is " + result10);
+
+var result11 = false - true;
+console.log("Result of result11 is " + result11);
+
+var result12 = 3 - 4;
+console.log("Result of result12 is " + result12);
+
+var result13 = "Bob" - "bill";
+console.log("Result of result13 is " + result13);
+
+var result14 = 5 >= 1;
+console.log("Result of result14 is " + result14);
+
+var result15 = 0 === 1;
+console.log("Result of result15 is " + result15);
+
+var result16 = 4 <= 1;
+console.log("Result of result16 is " + result16);
+
+var result17 = 1 != 1;
+console.log("Result of result17 is " + result17);
+
+var result18 = "A" > "B";
+console.log("Result of result18 is " + result18);
+
+var result19 = "B" < "C";
+console.log("Result of result19 is " + result19);
+
+var result20 = "a" > "A";
+console.log("Result of result20 is " + result20);
+
+var result21 = "b" < "A";
+console.log("Result of result21 is " + result21);
+
+var result22 = true === false;
+console.log("Result of result22 is " + result22);
+
+var result23 = true != true;
+console.log("Result of result23 is " + result23);
+
+
+console.log("Hi There! It's " + '"sunny"' + " out.");
---
+++ week-2/Exercises/JavaScript/Exercises Laussanne/ex.js
@@ -0,0 +1,4 @@
+for (i = 1; i < 11; i = i + 1)
+    {
+        console.log("Hello Powercoders! i = " + i)
+    };
